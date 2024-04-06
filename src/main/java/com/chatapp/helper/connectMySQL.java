b4c4c81220e626ca979a940f342efe5959d18809
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-04-06 21:54:57 +0700

    first commit

---
+++ src/main/java/com/chatapp/helper/connectMySQL.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chatapp.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Admin
+ */
+public class connectMySQL {
+    public static Connection getConnection() throws Exception{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String connectionUrl = "jdbc:mysql://localhost/chatapp";
+        String username = "root";
+        String password = "admin1234";
+        Connection con = DriverManager.getConnection(connectionUrl, username, password);
+        return con;
+    }
+}
